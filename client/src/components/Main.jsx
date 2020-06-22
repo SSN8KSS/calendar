d199e0f5ba08136902951eff00c19e263d225811
ozzychel <andrey.petunin86@gmail.com>
2020-06-22 11:28:38 -0700

    create client basic react components, install dependencies

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Main extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render () {
+    return (
+      <h2>Hello React</h2>
+    );
+  }
+}
