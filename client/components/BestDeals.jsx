e386476a80625c1ecc9e18d40969a30e9347941f
ozzychel <andrey.petunin86@gmail.com>
2020-06-22 16:25:36 -0700

    Implement GET req to the server, create basic components

---
+++ client/components/BestDeals.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BestDeals = () => (
+  <h3>BestDeals will be here</h3>
+);
+
+export default BestDeals;
---
+++ client/components/Calendar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Calendar = () => (
+  <h3>Calendar will be here</h3>
+);
+
+export default Calendar;
