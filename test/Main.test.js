a0e2643259bf75e41d8ff0db2b67fd03d2d7f2e7
ozzychel <andrey.petunin86@gmail.com>
2020-06-23 11:05:49 -0700

    create enzyme configuration, write more tests

--- test/Main.test.js
+++ test/Main.test.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import Main from '../client/components/Main.jsx';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
-configure({ adapter: new Adapter() });
-
-describe('Main component Testing', () => {
+describe('<Main/> component Testing', () => {
 
   let wrapper;
+  let instance;
 
   beforeEach(() => {
     wrapper = shallow(<Main />);
-  })
+    instance = wrapper.instance();
+  });
 
-  test('should have <h5> element with predefined text', () => {
+  test('should render 5 elements', () => {
+    expect(wrapper.children()).toHaveLength(5);
+  });
+
+  test('should have <h5> element with text', () => {
     expect(wrapper.find('h5').text()).toContain('people currently viewing');
   });
 
+  test('should have "currentHotel" state property', ()=>{
+    expect(instance.state.currentHotel).not.toBe(undefined);
+  });
 
+  test('"currentHotel" state property should be an empty array', ()=>{
+    expect(instance.state.currentHotel.length).toBe(0);
+  });
 
 });
