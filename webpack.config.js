b3aa483d20884cd680488426bea8dd4ca0f8dc69
ozzychel <andrey.petunin86@gmail.com>
2020-06-22 17:16:00 -0700

    upgrade configuration to use webpack 4.xx update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,22 @@ const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
-  },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'env']
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
         }
       }
     ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
   }
 };
