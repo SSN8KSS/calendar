27194296d0f4b82dbde23e7c93562025c77a6fbd
ozzychel <andrey.petunin86@gmail.com>
2020-06-26 13:21:31 -0700

    create helper func that checks vacancy and prices

--- client/lib/getUpdatedDataFromServer.js
+++ client/lib/getUpdatedDataFromServer.js
@@ -5,6 +5,7 @@ const getUpdatedDataFromServer = async (param) => {
   console.log(param)
   const response = await
   axios.get(`http://localhost:8080/api/calendar/update/?checkIn=${param.checkIn}&checkOut=${param.checkOut}&guestsNumber=${param.guestsNumber}&id=${param.id}&roomsNumber=${param.roomsNumber}`);
+  console.log('RESPONSE',response.data)
   return response.data;
 };
 
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const port = 8080;
 const morgan = require('morgan');
 const parser = require('body-parser');
 const db = require('../database/index.js');
+const moment = require('moment');
 
 app.listen(port, () => console.log(`App listening on http://localhost:${port}`));
 
@@ -45,41 +46,54 @@ app.get('/api/calendar/update/', (req, res) => {
 });
 
 const sendResponseWithUpdatedData = (data, req, res) => {
-  // console.log(req.query.checkin);
-  const checkInDate = req.query.checkIn;
-  const checkOutDate = req.query.checkOut;
+  const checkInDate = moment(req.query.checkIn).format('YYYY-MM-DD');
+  const checkOutDate = moment(req.query.checkOut).format('YYYY-MM-DD');
   const guestsNumber = req.query.guestsNumber;
   const dataItem = data[0];
   let roomsNumber = req.query.roomsNumber;
-  console.log(checkInDate, checkOutDate, guestsNumber, roomsNumber);
-  //check if room quantity < than rooms available --> send null
-  //check if guest amount is samller than maxguests -- if not upgrade roomsnumber
-  const rej = {err_msg: ''};
-  const update = {msg: ''};
+  let response = true;
+  let newData = [...data];
+  let rej = {err_msg: ''};
+  let totalNights;
 
   if (dataItem.maxGuestPerRoom < guestsNumber) {
     roomsNumber = Math.ceil(guestsNumber / dataItem.maxGuestPerRoom);
-    update['roomsNumber'] = roomsNumber;
-    update['msg'] = '<roomsNumber updated>';
   }
 
   if (dataItem.roomsTotal < roomsNumber) {
-    rej['err_msg'] = 'over the limit of rooms available at the property';
-    res.status(200).send(rej);
+    rej['err_msg'] = '<over the limit of rooms available at the property>';
+    response = false;
   }
 
+  let checkInIndex;
+  let checkOutIndex;
+  for (let i = 0; i < dataItem.vacancy.length; i ++) {
+    if (dataItem.vacancy[i].date === checkInDate) {
+      checkInIndex = i;
+    }
+    if (dataItem.vacancy[i].date === checkOutDate) {
+      checkOutIndex = i;
+    }
+  }
+  let timeGap = dataItem.vacancy.slice(checkInIndex, checkOutIndex);
+  console.log(timeGap);
+  totalNights = timeGap.length;
+  for (let j = 0; j < timeGap.length; j++) {
+    if (timeGap[j].isBooked) {
+      rej['err_msg'] += '<your dates are not available>';
+      response = false;
+      break;
+    }
+  }
+  for (let k = 0; k < newData[0].prices.length; k++) {
+    newData[0].prices[k].price *= totalNights;
+  }
 
-  // for (let i = 0; i < dataItem.vacancy.length;) {
-  //   //check if check in date avail
-  //   //check if checkout date avail
-  //   //check dates between avail
-
-  // }
-
-
-
-
-  // res.status(200).send(data);
+  if (response) {
+    res.status(200).send(newData);
+  } else {
+    res.status(200).send(rej);
+  }
 }
 
 module.exports = app;
