ceccef8691a44f7a2253d72fda5837088aa58a38
ozzychel <andrey.petunin86@gmail.com>
2020-06-25 17:05:20 -0700

    implement view-all functionality

--- client/components/AllDeals.jsx
+++ client/components/AllDeals.jsx
@@ -1,31 +1,132 @@
 import React from 'react';
 
-const AllDeals = ({currentHotel}) => {
-  if (currentHotel.length === 0) {
-    return ( <div>Loading data...</div>);
-  } else {
-    const displaySix = [];
+class AllDeals extends React.Component {
+  constructor (props) {
+    super(props);
 
-    for (var i = 0; i < 6; i++) {
-      displaySix.push(
-        <div className="allDeals-line" key={i}>
-          <span>{currentHotel[0].prices[i].serviceName}</span>
-          <span>{currentHotel[0].prices[i].price}</span>
-        </div>);
+    this.state = {
+      isClicked: false
+    };
+
+    this.renderSix = this.renderSix.bind(this);
+    this.onClickHandler = this.onClickHandler.bind(this);
+  }
+
+  renderSix (hotels) {
+    const displaySix = [];
+    if (hotels.length === 0) {
+      return ( <div>Loading data...</div>);
+    } else {
+      for (var i = 0; i < hotels[0].prices.length; i++) {
+        if (hotels[0].prices[i].price !== 0) {
+          displaySix.push(
+            <div className="allDeals-line" key={i}>
+              <span>{hotels[0].prices[i].serviceName}</span>
+              <span>{hotels[0].prices[i].price}</span>
+            </div>);
+        }
       }
+      return displaySix.slice(0, 6);
+    }
+  }
+
+  onClickHandler () {
+    this.setState({
+      isClicked: !this.state.isClicked
+    });
+  }
+
+  renderAll () {
+    let hotels = this.props.currentHotel;
+    if (hotels.length === 0) {
+      return (<div></div>);
+    } else {
+      let rates = [];
+      for (var i = 0; i < hotels[0].prices.length; i++) {
+        rates.push(
+          <div key={i}>
+            <span className="viewAll-span">{hotels[0].prices[i].serviceName}</span>
+            <span className="viewAll-span">{hotels[0].prices[i].price}</span>
+          </div>
+        );
+      }
+      return rates;
+    }
+  }
+
+  render () {
     return (
       <div className="allDeals-container">
+
         <div className="allDeals-grid">
-       {displaySix}
+          {this.renderSix(this.props.currentHotel)}
         </div>
 
-      <div style={{'text-align':'center'}}>View all deals</div>
-      <div style={{'text-align':'center'}}>Comment</div>
+        <div style={{'textAlign': 'center'}}>
+          <button type="button" onClick={this.onClickHandler}>View All</button>
 
+          <div className={`viewAll${
+            this.state.isClicked ? '' : '-disabled'
+          }`}>
+            {this.renderAll()}
+          </div>
+        </div>
       </div>
     );
-
   }
 }
+
+// const AllDeals = ({currentHotel}) => {
+//   let isClicked = false;
+//   const renderSix = (hotels) => {
+//     const displaySix = [];
+//     if (hotels.length === 0) {
+//       return ( <div>Loading data...</div>);
+//     } else {
+//       for (var i = 0; i < hotels[0].prices.length; i++) {
+//         if (hotels[0].prices[i].price !== 0) {
+//           displaySix.push(
+//             <div className="allDeals-line" key={i}>
+//               <span>{hotels[0].prices[i].serviceName}</span>
+//               <span>{hotels[0].prices[i].price}</span>
+//             </div>);
+//         }
+//       }
+//       return displaySix.slice(0, 6);
+//     }
+//   };
+
+//   const renderAll = () => {
+//     console.log('called')
+//     isClicked = true;
+//   }
+
+//   return (
+//     <div className="allDeals-container">
+//       <div className="allDeals-grid">
+//         {renderSix(currentHotel)}
+//       </div>
+
+//       <div style={{'textAlign': 'center'}}>
+//         <button type="submit" onClick={renderAll}>View all</button>
+//         <div className={`viewAll${
+//           isClicked ? '' : '-disabled'
+//         }`}>
+//           <span>LINE</span>
+//           <span>LINE</span>
+//           <span>LINE</span>
+//           <span>LINE</span>
+//           <span>LINE</span>
+//           <span>LINE</span>
+//         </div>
+//       </div>
+
+//       <div style={{'textAlign': 'center'}}>Comment</div>
+//     </div>
+//   );
+
+
+// };
+
 
 export default AllDeals;
